9c8e7b7982d4a8f76cffe48d7b110f323bb72a14
yujiosaka <yuji.isobe@emin.co.jp>
2017-12-10 12:37:39 +0900

    Add pluggable cache

--- examples/multiple-queue.js
+++ examples/multiple-queue.js
@@ -4,15 +4,14 @@ HCCrawler.launch({
   evaluatePage: (() => ({
     title: $('title').text(),
     h1: $('h1').text(),
-    p: $('p').text(),
   })),
   onSuccess: (result => {
     console.log('onSuccess', result);
   }),
 })
   .then(crawler => {
-    crawler.queue('https://example.com'); // one URL
-    crawler.queue(['https://example.net', { url: 'https://example.org' }]); // multiple URLs in different styles.
+    crawler.queue('https://example.com/'); // one URL
+    crawler.queue(['https://example.net/', { url: 'https://example.org/' }]); // multiple URLs in different styles.
     crawler.onIdle()
       .then(() => crawler.close());
   });
--- examples/priority-queue.js
+++ examples/priority-queue.js
@@ -5,16 +5,15 @@ HCCrawler.launch({
   evaluatePage: (() => ({
     title: $('title').text(),
     h1: $('h1').text(),
-    p: $('p').text(),
   })),
   onSuccess: (result => {
     console.log('onSuccess', result);
   }),
 })
   .then(crawler => {
-    crawler.queue({ url: 'https://example.com' }); // First queue will be requested first regardless of priority
-    crawler.queue({ url: 'https://example.net', priority: 1 });
-    crawler.queue({ url: 'https://example.org', priority: 2 }); // This queue is requested before the previous queue
+    crawler.queue({ url: 'https://example.com/' }); // First queue will be requested first regardless of priority
+    crawler.queue({ url: 'https://example.net/', priority: 1 });
+    crawler.queue({ url: 'https://example.org/', priority: 2 }); // This queue is requested before the previous queue
     crawler.onIdle()
       .then(() => crawler.close());
   });
