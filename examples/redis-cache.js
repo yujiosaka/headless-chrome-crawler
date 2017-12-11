7a29c0e89d560f81c516177d98b14234240e99f9
yujiosaka <yuji.i@doboken.net>
2017-12-12 03:01:07 +0900

    Elaborate redis cache example

--- examples/redis-cache.js
+++ examples/redis-cache.js
@@ -3,8 +3,6 @@ const RedisCache = require('headless-chrome-crawler/cache/redis');
 
 function launch(persistCache) {
   return HCCrawler.launch({
-    maxConcurrency: 1,
-    maxRequest: 2,
     evaluatePage: (() => ({
       title: $('title').text(),
       h1: $('h1').text(),
@@ -21,7 +19,6 @@ launch(true) // Launch the crawler with persisting cache
   .then(crawler => {
     crawler.queue('https://example.com/');
     crawler.queue('https://example.net/');
-    crawler.queue('https://example.org/'); // The queue won't be requested due to maxRequest option
     return crawler.onIdle()
       .then(() => crawler.close()); // Close the crawler but cache won't be cleared
   })
