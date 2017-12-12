8a510d78e445b0fc29b953d6b841e481a74f49d8
yujiosaka <yuji.isobe@emin.co.jp>
2017-12-13 02:27:36 +0900

    More description for conditional screenshot

--- examples/conditional-screenshot.js
+++ examples/conditional-screenshot.js
@@ -10,6 +10,7 @@ HCCrawler.launch({
     if (!result.options.saveAs) return;
     console.log(`Screenshot is saved as ${PATH}${result.options.saveAs}`);
   }),
+  // Passing a function which resolves screenshot options
   screenshot: (options => {
     if (!options.saveAs) return null;
     return { path: `${PATH}${options.saveAs}` };
@@ -17,6 +18,7 @@ HCCrawler.launch({
 })
   .then(crawler => {
     crawler.queue({ url: 'https://example.com/' });
+    // saveAd is a custom option added for onSuccess and screenshot callbacks
     crawler.queue({ url: 'https://example.net/', saveAs: 'example-net.png' });
     crawler.queue({ url: 'https://example.org/', saveAs: 'example-org.png' });
     crawler.onIdle()
