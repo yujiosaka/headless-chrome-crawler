cc415ecca7295f327e9453af8b163cb03572d622
yujiosaka <yuji.isobe@emin.co.jp>
2017-12-13 15:20:43 +0900

    collect links

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   "extends": "airbnb-base",
+  "globals": {
+    'window': true,
+  },
   "rules": {
     "class-methods-use-this": 0,
     "arrow-parens": ["warn", "as-needed"],
     "no-underscore-dangle": 0,
     "no-console": 0,
     "no-void": 0,
+    "no-param-reassign": 0,
   },
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+- Automatically collecting and following links.
+
 ## [1.2.1] - 2017-12-13
 ### Added
 
